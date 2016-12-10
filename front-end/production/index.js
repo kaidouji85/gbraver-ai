9e1633d348983ace7ad42fa7519079a887f38080
kaidouji85 <kaidouji85@gmail.com>
2016-12-10 15:04:49 +0900

    フロントエンドビルドにwatchを追加した

--- front-end/production/index.js
+++ front-end/production/index.js
@@ -13,7 +13,7 @@ window.onload = function() {
   }
 
   function create () {
-
+    // テストでコメントを追加したよ
     var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
     logo.anchor.setTo(0.5, 0.5);
 
