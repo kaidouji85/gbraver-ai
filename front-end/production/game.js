be7fb63a0a9428a0d5423c1cad8e76897f4df79b
kaidouji85 <kaidouji85@gmail.com>
2016-12-10 15:57:26 +0900

    画面をモバイルサイズに合うようにした

---
+++ front-end/production/game.js
@@ -0,0 +1,17 @@
+let game = new Phaser.Game(320, 568, Phaser.AUTO, '', {
+  preload: function() {
+    // ゲームコンテンツを画面中央、サイズ最大で表示するための設定
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+    this.scale.refresh();
+
+    this.load.image('logo', 'phaser.png');
+  },
+
+  create: function() {
+    var logo = this.add.sprite(this.world.centerX, this.world.centerY, 'logo');
+    logo.anchor.setTo(0.5, 0.5);
+  }
+});
+
+module.exports = game;
